2009277cca243540d34684bbb6d6c90bc5d6517d
Matyáš Frank <matyas.frank@ackee.cz>
2026-06-03 15:45:54 +0200

    ✨ Add StravaApplication and Koin bootstrap

---
+++ app/src/main/java/cz/ackee/strava/wearos/StravaApplication.kt
@@ -0,0 +1,19 @@
+package cz.ackee.strava.wearos
+
+import android.app.Application
+import cz.ackee.strava.wearos.core.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class StravaApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@StravaApplication)
+            modules(appModule)
+        }
+    }
+}
