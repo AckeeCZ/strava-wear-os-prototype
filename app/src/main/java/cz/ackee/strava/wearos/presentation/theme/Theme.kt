5db5a71aaf993d8c4fba7dacdd0382d400d97da4
Matyáš Frank <matyas.frank@ackee.cz>
2026-06-03 12:48:49 +0200

    Initial commit

---
+++ app/src/main/java/cz/ackee/strava/wearos/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package cz.ackee.strava.wearos.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun AckeeStravaWearOSTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
