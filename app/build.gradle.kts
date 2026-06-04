12b6b20392639da7ccf8374876373d256ec9bd7a
Matyáš Frank <matyas.frank@ackee.cz>
2026-06-04 13:19:52 +0200

    ✨ Add android-maps-utils dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.maps.utils)
     implementation(libs.navigation3.runtime)
     implementation(libs.navigation3.ui)
     implementation(libs.wear.compose.navigation3)
