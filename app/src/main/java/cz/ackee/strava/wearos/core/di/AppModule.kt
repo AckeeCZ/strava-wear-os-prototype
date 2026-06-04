795a42cba4e341bed3b21337861f4af5bdb4bd1f
Matyáš Frank <matyas.frank@ackee.cz>
2026-06-04 13:26:53 +0200

    ✨ Add routes Koin module

--- app/src/main/java/cz/ackee/strava/wearos/core/di/AppModule.kt
+++ app/src/main/java/cz/ackee/strava/wearos/core/di/AppModule.kt
@@ -1,5 +1,8 @@
 package cz.ackee.strava.wearos.core.di
 
+import cz.ackee.strava.wearos.feature.routes.routesModule
 import org.koin.dsl.module
 
-val appModule = module {}
+val appModule = module {
+    includes(routesModule)
+}
---
+++ app/src/main/java/cz/ackee/strava/wearos/feature/routes/RoutesModule.kt
@@ -0,0 +1,13 @@
+package cz.ackee.strava.wearos.feature.routes
+
+import cz.ackee.strava.wearos.feature.routes.data.MockRouteRepository
+import cz.ackee.strava.wearos.feature.routes.domain.RouteRepository
+import cz.ackee.strava.wearos.feature.routes.presentation.list.RoutesListViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val routesModule = module {
+    single<RouteRepository> { MockRouteRepository(androidContext()) }
+    viewModel { RoutesListViewModel(repository = get()) }
+}
