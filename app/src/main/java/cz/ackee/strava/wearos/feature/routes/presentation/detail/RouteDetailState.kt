8b306e065550e23d1c55f7beae5bfc2b1b96814c
Matyáš Frank <matyas.frank@ackee.cz>
2026-06-05 09:43:42 +0200

    ✨ Add route detail state and view model

---
+++ app/src/main/java/cz/ackee/strava/wearos/feature/routes/presentation/detail/RouteDetailState.kt
@@ -0,0 +1,9 @@
+package cz.ackee.strava.wearos.feature.routes.presentation.detail
+
+import cz.ackee.strava.wearos.feature.routes.domain.Route
+
+sealed interface RouteDetailState {
+    data object Loading : RouteDetailState
+    data class Content(val route: Route) : RouteDetailState
+    data object Error : RouteDetailState
+}
